70d96296eb6b1b2532d09b74a26dbeb647621b47
mackcross <mackenzie-cross@uiowa.edu>
2023-05-04 17:08:09 -0500

    refining maps

--- sci_comm/data/reducedlunch_clean.R
+++ sci_comm/data/reducedlunch_clean.R
@@ -5,7 +5,7 @@ library(dplyr)
 library(rgdal)
 
 # Fill in missing csv values
-df <- read_csv("Documents/research/primate_map/project_materials/sci_comm/data/reducedmeals.csv")
+df <- read_csv("reducedmeals.csv")
 
 df$county_name <- gsub("'", "", df$county_name)
 df$county_name <- gsub("OBrien", "Obrien", df$county_name)
@@ -16,7 +16,7 @@ df_filled <- df %>%
   mutate(county_name = ifelse(is.na(county_name), county_name[!is.na(county_name)][1], county_name))
 
 
-my_shapefile <- readOGR(dsn = "Documents/research/primate_map/project_materials/sci_comm/shapefiles/original_Iowa/IowaCounties.shp")
+my_shapefile <- readOGR(dsn = "~/Documents/mackcross/sci_comm/shapefiles/original_Iowa/IowaCounties.shp")
 
 
 merged_df <- merge(df_filled, my_shapefile@data[c("CountyName", "OBJECTID")], 
@@ -27,7 +27,7 @@ merged_df$k12_enrollment <- as.numeric(merged_df$k12_enrollment)
 
 grouped <- merged_df %>%
   group_by(OBJECTID, county_name, year) %>%
-  summarize(import_ratio = sum(k12_enrollment, na.rm = TRUE) / sum(total_reduced_free, na.rm = TRUE),
+  summarize(ratio = sum(k12_enrollment, na.rm = TRUE) / sum(total_reduced_free, na.rm = TRUE),
             .groups = "drop")
 head(grouped)
 
