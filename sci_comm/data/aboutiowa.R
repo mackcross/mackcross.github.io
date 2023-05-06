de551b586c9a94bef9bfa55190d80f3be4859745
mackcross <mackenzie-cross@uiowa.edu>
2023-05-06 12:04:52 -0500

    mapping

---
+++ sci_comm/data/aboutiowa.R
@@ -0,0 +1,38 @@
+rm(list=ls())
+library(readr)
+
+# Existing datasets
+meals_df <- read_csv("Documents/mackcross/sci_comm/data/cleaned_grouped_data.csv")
+edu_df <- read_csv("Documents/mackcross/sci_comm/data/cleaned_rural.csv")
+
+# Rural variables 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
